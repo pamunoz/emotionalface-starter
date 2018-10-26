11807dbbee3036ee3c3e10d740d484c7867665e7
pamunoz <pfariasmunoz@gmail.com>
2018-10-26 10:40:22 -0300

    add Custom View

--- app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
@@ -32,11 +32,18 @@ package com.raywenderlich.emotionalface
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    // 1
+    val textView = TextView(this)
+    // 2
+    textView.text = "Hello Custom Views"
+    // 3
+    setContentView(textView)
   }
 }
