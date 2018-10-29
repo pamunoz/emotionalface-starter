50364fa3af8e42c02eec1b66d45b565265597d4a
pamunoz <pfariasmunoz@gmail.com>
2018-10-29 10:37:53 -0300

    initialize the Paint property with some Colors

--- app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
@@ -1,9 +1,24 @@
 package com.raywenderlich.emotionalface
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    // Paint object for coloring and styling
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    // Some colors for the face background, eyes and mouth.
+    private var faceColor = Color.YELLOW
+    private var eyesColor = Color.BLACK
+    private var mouthColor = Color.BLACK
+    private var borderColor = Color.BLACK
+    // Face border width in pixels
+    private var borderWidth = 4.0f
+    // View size in pixels
+    private var size = 320
+
 
 }
