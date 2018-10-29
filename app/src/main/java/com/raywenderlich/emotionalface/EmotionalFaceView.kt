a9af696c4bc5c274ba67dfa2650b698b37ebd7f0
pamunoz <pfariasmunoz@gmail.com>
2018-10-29 10:48:09 -0300

    Draw the eyes

--- app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -54,6 +55,25 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     }
 
     private fun drawEyes(canvas: Canvas) {
+        // 1 Set the paint color to the eyesColor and make it fill the drawing area.
+        paint.color = eyesColor
+        paint.style = Paint.Style.FILL
+
+        // 2 Create a RectF object with left, top, right and bottom using the following
+        // percentages of the size: (32%, 23%, 43%, 50%). Then you draw the left eye by
+        // drawing an oval with the created RectF. For more info about RectF, check the
+        // docs. [https://developer.android.com/reference/android/graphics/RectF.html]
+        val leftEyeRect =
+                RectF(size * 0.32f, size * 0.23f, size * 0.43f, size * 0.50f)
+
+        canvas.drawOval(leftEyeRect, paint)
+
+        // 3 Do the same as the last step but with the following percentages of the size:
+        // (57%, 23%, 68%, 50%)
+        val rightEyeRect =
+                RectF(size * 0.57f, size * 0.23f, size * 0.68f, size * 0.50f)
+
+        canvas.drawOval(rightEyeRect, paint)
     }
 
     private fun drawMouth(canvas: Canvas) {
