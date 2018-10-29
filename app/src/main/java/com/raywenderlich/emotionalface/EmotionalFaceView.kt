48e3997a243331cb431efe06aaee9008255f740f
pamunoz <pfariasmunoz@gmail.com>
2018-10-29 10:44:25 -0300

    Draw the face background

--- app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
@@ -31,6 +31,26 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     }
 
     private fun drawFaceBackground(canvas: Canvas) {
+        // 1 Set the paint color to the faceColor and make it fill the drawing area.
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        // 2 Calculate a radius for a circle which you want to draw as the face background.
+        val radius = size / 2f
+
+        // 3 Draw the background circle with a center of (x,y), where x and y are equal to the half
+        // of size, and with the calculated radius.
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        // 4 Change the paint color to the borderColor and make it just draw a border around the
+        // drawing area by setting the style to STROKE.
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        // 5 Draw a border with the same center but with a radius shorter than the previous
+        // radius by the borderWidth.
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth / 2f, paint)
     }
 
     private fun drawEyes(canvas: Canvas) {
