a47a2651dce00d2b81bc4c5f525ac7d8a7b1f21f
pamunoz <pfariasmunoz@gmail.com>
2018-10-29 10:36:10 -0300

    Add the customview to the layout

---
+++ app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.emotionalface
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+}
