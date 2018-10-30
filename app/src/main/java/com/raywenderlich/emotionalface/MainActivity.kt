284fc4a877b191dd15514c292daed30aef4e239f
pamunoz <pfariasmunoz@gmail.com>
2018-10-30 07:20:00 -0300

    Add kotling extension for interacting with the views

--- app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
@@ -32,6 +32,7 @@ package com.raywenderlich.emotionalface
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
